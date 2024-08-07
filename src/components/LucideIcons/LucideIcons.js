0f8f572c7a6ea414f93f408826135dab33d35396
bilalseen <bilalsen2010@gmail.com>
2024-08-07 11:46:52 +0300

    fix: Handle missing LucideIcon gracefully in Icon component

--- src/components/LucideIcons/LucideIcons.js
+++ src/components/LucideIcons/LucideIcons.js
@@ -3,6 +3,11 @@ import { icons } from "lucide-react-native";
 const Icon = ({ name, color, size }) => {
   const LucideIcon = icons[name];
 
+  if (!LucideIcon) {
+    console.error(`Icon "${name}" does not exist in the icons object.`);
+    return null;
+  }
+
   return <LucideIcon color={color} size={size} />;
 };
 
