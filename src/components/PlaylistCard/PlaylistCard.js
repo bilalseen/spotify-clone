82e3c99349ac3348aba5af10bc93c5ddab325fac
bilalseen <bilalsen2010@gmail.com>
2024-08-10 11:47:41 +0300

    refactor: Add PlaylistCard component for displaying playlist information with image and title

---
+++ src/components/PlaylistCard/PlaylistCard.js
@@ -0,0 +1,36 @@
+import { Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+
+import styles from "./PlaylistCard.style";
+
+export default function PlaylistCard({ item }) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        item.type == "artist" && {
+          alignItems: "center",
+        },
+      ]}
+    >
+      <Image
+        style={[
+          styles.cardImage,
+          item.type == "artist" && { borderRadius: 100 },
+        ]}
+        source={{
+          uri: item.coverImage,
+        }}
+      />
+      <Text
+        style={[
+          styles.cardTitle,
+          item.type == "artist" && { textAlign: "center" },
+        ]}
+        numberOfLines={2}
+      >
+        {item.type == "artist" ? item.name : item.description}
+      </Text>
+    </TouchableOpacity>
+  );
+}
