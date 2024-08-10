2a9471d4f2ee0aab89c318fd01a0725307e7928d
bilalseen <bilalsen2010@gmail.com>
2024-08-10 11:49:12 +0300

    refactor: Add PlayerReducer to Store configuration

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ProfileReducer from "./Profile/ProfileSlice";
 import PlaylistReducer from "./Playlist/PlaylistSlice";
+import PlayerReducer from "./Player/PlayerSlice";
 
 const Store = configureStore({
   reducer: {
     profile: ProfileReducer,
     playlist: PlaylistReducer,
+    player: PlayerReducer,
   },
 });
 
