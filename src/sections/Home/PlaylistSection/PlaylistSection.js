2dcc7d2fb22b77bd591fbaeb7cfd31ec8620cf80
bilalseen <bilalsen2010@gmail.com>
2024-08-25 14:24:32 +0300

    feat: Add PlaylistSection component for displaying playlists in the Home screen

---
+++ src/sections/Home/PlaylistSection/PlaylistSection.js
@@ -0,0 +1,25 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+
+import styles from "./PlaylistSection.style";
+
+import PlaylistCard from "../../../components/Home/PlaylistCard";
+
+export default function PlaylistSection({ data }) {
+  const ItemSeparator = () => {
+    return <View style={{ width: 10 }} />;
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{data.title}</Text>
+      <FlatList
+        data={data.playlists}
+        renderItem={({ item }) => <PlaylistCard item={item} />}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={ItemSeparator}
+      />
+    </View>
+  );
+}
---
+++ src/sections/Home/PlaylistSection/PlaylistSection.style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "auto",
+    gap: 10,
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "600",
+    color: "#fff",
+  },
+});
+
+export default styles;
