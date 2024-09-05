be5a6a4cb59f0428fc6c7d3f6a24addfdf60558e
bilalseen <bilalsen2010@gmail.com>
2024-09-05 18:02:23 +0300

    refactor: Update PlaylistSection component to dynamically adjust the number of columns based on the library listing type

--- src/sections/Home/PlaylistSection/PlaylistSection.js
+++ src/sections/Home/PlaylistSection/PlaylistSection.js
@@ -4,11 +4,15 @@ import React from "react";
 import styles from "./PlaylistSection.style";
 
 import PlaylistCard from "../../../components/Home/PlaylistCard";
+import { useSelector } from "react-redux";
 
 export default function PlaylistSection({ data }) {
+  const libraryListingType = useSelector((state) => state.library.listingType);
+
   const ItemSeparator = () => {
     return <View style={{ width: 10 }} />;
   };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{data.title}</Text>
@@ -17,6 +21,7 @@ export default function PlaylistSection({ data }) {
         renderItem={({ item }) => <PlaylistCard item={item} />}
         keyExtractor={(item) => item.id}
         horizontal
+        numColumns={libraryListingType === "Grid" ? 3 : 1}
         showsHorizontalScrollIndicator={false}
         ItemSeparatorComponent={ItemSeparator}
       />
