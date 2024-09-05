201a461898b85de0515d69d6349b169b890e830c
bilalseen <bilalsen2010@gmail.com>
2024-09-05 19:05:42 +0300

    refactor: Add onPress prop to CustomSignButton component

--- src/components/SignUp/CustomSignButton/CustomSignButton.js
+++ src/components/SignUp/CustomSignButton/CustomSignButton.js
@@ -3,13 +3,19 @@ import React from "react";
 import styles from "./CustomSignButton.style";
 import LucideIcons from "../../global/LucideIcons";
 
-export default function CustomSignButton({ iconSource, text, bckgColor }) {
+export default function CustomSignButton({
+  iconSource,
+  text,
+  bckgColor,
+  onPress,
+}) {
   return (
     <TouchableOpacity
       style={[
         styles.container,
         { backgroundColor: bckgColor, borderWidth: bckgColor ? 0 : 1 },
       ]}
+      onPress={onPress}
     >
       {iconSource && <Image source={iconSource} style={styles.icon} />}
 
