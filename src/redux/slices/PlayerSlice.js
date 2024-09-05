f413980b99ac88f4281f26055229ea0de90d6355
bilalseen <bilalsen2010@gmail.com>
2024-09-05 21:37:22 +0300

    refactor: Update PlayerSlice to include additional song properties and control options

--- src/redux/slices/PlayerSlice.js
+++ src/redux/slices/PlayerSlice.js
@@ -2,12 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   isPlaying: false,
-  isAdded: false,
   currentTime: 0,
-  songName: "Song Name",
-  artistName: "Artist Name",
-  songImage: "https://via.placeholder.com/150",
+  songName: "Ride",
+  artistName: "Twenty One Pilots",
+  songImage: "https://i.scdn.co/image/ab67616d00001e022df0d98a423025032d0db1f7",
   songUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  isPlayingOnAnotherDevice: false,
+  isLiked: true,
+  shuffleOn: false,
+  repeat: "off",
 };
 
 const playerSlice = createSlice({
@@ -29,9 +32,31 @@ const playerSlice = createSlice({
     remove(state) {
       state.isAdded = false;
     },
+    setIsPlayingOnAnotherDevice(state, action) {
+      state.isPlayingOnAnotherDevice = action.payload;
+    },
+    setLiked(state) {
+      state.isLiked = !state.isLiked;
+    },
+    setShuffle(state) {
+      state.shuffleOn = !state.shuffleOn;
+    },
+    setRepeat(state, action) {
+      state.repeat = action.payload.state;
+    },
   },
 });
 
-export const { play, pause, add, remove, setCurrentTime } = playerSlice.actions;
+export const {
+  play,
+  pause,
+  add,
+  remove,
+  setCurrentTime,
+  setIsPlayingOnAnotherDevice,
+  setLiked,
+  setShuffle,
+  setRepeat,
+} = playerSlice.actions;
 
 export default playerSlice.reducer;
