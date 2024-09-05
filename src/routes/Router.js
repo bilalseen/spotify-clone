4b822881bad5d71af3e633e9bf92831a82b6d6c0
bilalseen <bilalsen2010@gmail.com>
2024-09-05 21:41:59 +0300

    refactor: Update Router.js to include SignUp and PlaybackControl screens

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,29 +1,45 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import BottomTabNavigation from "./BottomTabNavigation";
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import Store from "../redux/Store";
+import SignUp from "../screens/SignUp";
+import PlaybackControl from "../screens/PlaybackControl";
 
 const Stack = createNativeStackNavigator();
 
+const AppNavigator = ({ userLoggedIn }) => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        {userLoggedIn ? (
+          <>
+            <Stack.Screen name="Home" component={BottomTabNavigation} />
+            <Stack.Screen name="PlaybackControl" component={PlaybackControl} />
+          </>
+        ) : (
+          <Stack.Screen name="SignUp" component={SignUp} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
 const Router = () => {
+  const userLoggedIn = useSelector((state) => state.profile.loggedIn);
+  return <AppNavigator userLoggedIn={userLoggedIn} />;
+};
+
+const RootComponent = () => {
   return (
     <Provider store={Store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen
-            name="BottomTabNavigation"
-            component={BottomTabNavigation}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Router />
     </Provider>
   );
 };
 
-export default Router;
+export default RootComponent;
