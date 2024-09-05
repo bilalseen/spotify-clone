6b440011b282b5f57669befe294345b4b05f3f58
bilalseen <bilalsen2010@gmail.com>
2024-09-05 21:37:30 +0300

    feat: Add LibraryReducer to manage library state

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import ProfileReducer from "./slices/ProfileSlice";
 import PlaylistReducer from "./slices/PlaylistSlice";
 import PlayerReducer from "./slices/PlayerSlice";
+import LibraryReducer from "./slices/LibrarySlice";
 
 const Store = configureStore({
   reducer: {
     profile: ProfileReducer,
     playlist: PlaylistReducer,
     player: PlayerReducer,
+    library: LibraryReducer,
   },
 });
 
